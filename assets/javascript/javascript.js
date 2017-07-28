522a4f5b503285a67609d4e639e319e8cea9e825
Ritter Gustave <rGustave5@gmail.com>
2017-07-27 21:10:34 -0700

    Succes on AJAX data acquisition

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,29 +1,32 @@
 $(document).ready(function() {
 
 $("#search").on("click", function() {
+  // defining values the user wants
   var searchTerm = $("#searchTerm").val();
   var numRecords = $("#num-records").val();
-  var beginDate = $("#begin-date").val();
-  var endDate = $("#end-date").val();
-  var apiKey = "7032c5448f4445509a3f0dd6549fec88"
+  var beginDate = $("#begin-date").val() + "0101";
+  var endDate = $("#end-date").val() + "0727";
+  var apiKey = "7032c5448f4445509a3f0dd6549fec88";
   var sortOption = $("#sortOptions").attr("value");
 
   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=" + apiKey + "&fq=" + numRecords + "&begin_date=" + beginDate + "&end_date=" + endDate + "&sort=" + sortOption;
-
+  console.log(queryURL);
   $.ajax({
-    method:"GET",
-    url:queryURL
+    url: queryURL,
+    method: "GET"
   }).done (function(response) {
+
+    console.log(response);
+    console.log("hello");
     for(var i=0; i<numRecords; i++) {
       var newDiv = $("<div>")
-      newDiv.append("<p>" + response.docs[i].headline.main + "</p>");
-      newDiv.append("<p>" + response.docs[i].byline.original + "</p>");
-      newDiv.append("<p>" + response.docs[i].section_name + "</p>");
-      newDiv.append("<p>" + response.docs[i].pub_date + "</p>");
-      newDiv.append("<p>" + response.docs[i].web_url + "</p>");
-      $("article-area").append(newDiv);
-      newDiv.append("<p>" + response.docs[i].snippet + "</p>");
-      $("article-area").append(newDiv);
+      newDiv.append("<p>" + response.response.docs[i].headline.main + "</p>");
+      // newDiv.append("<p>" + response.response.docs[i].byline.original + "</p>");
+      newDiv.append("<p>" + response.response.docs[i].section_name + "</p>");
+      newDiv.append("<p>" + response.response.docs[i].pub_date + "</p>");
+      newDiv.append("<p>" + response.response.docs[i].web_url + "</p>");
+      newDiv.append("<p>" + response.response.docs[i].snippet + "</p>");
+      $("#article-area").append(newDiv);
     }
   });
 })
